ec8071a0c26062e78475b9f63f8bf8ba1ce9bfed
Luke Jones <nnblc565@gmail.com>
2021-12-09 13:00:13 +1030

    Added a game timer

---
+++ Assets/Scripts/UI/GameTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class GameTimer : MonoBehaviour
+{
+    public TextMeshProUGUI _timeText = null;
+
+    private void Awake()
+    {
+        _timeText = GetComponent<TextMeshProUGUI>();
+        _timeText.text = "99";
+    }
+
+    private void LateUpdate()
+    {   //Make sure there is an instance
+        if (!GameManager.s_instance || GameManager.s_instance.m_startTime == 0)
+            return;
+
+        GameManager m = GameManager.s_instance;
+        _timeText.text = ((int)(m.m_startTime + m._maxGameTime - Time.time)).ToString();
+    }
+}
