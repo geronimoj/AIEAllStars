19d27e3b9382fb6e916bb3acbaa73e3b4d2d141f
Joshua Tan <joshuatan0806@gmail.com>
2021-12-09 16:54:38 +1030

    merged test ai and player

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -37,6 +37,49 @@ public class Player : MonoBehaviour
 
     Animator animator;
 
+    [Header("AI Customisation")]
+
+    [Space]
+    public bool IsAI = true;
+    protected int MoveInput = 0;
+
+    [Space]
+    public TimerData MoveData;
+    public TimerData JumpData;
+    public TimerData DashData;
+    public TimerData SkillData;
+
+    [Space]
+    public float AttackRange = 1;
+
+    [System.Serializable]
+    public class TimerData
+    {
+        public float MinTime = 3;
+        public float MaxTime = 5;
+        protected float CurrentTime = 0;
+
+        public bool CallTimer()
+        {
+            if (CurrentTime > 0)
+            {
+                CurrentTime -= Time.deltaTime;
+                return false;
+            }
+            else
+            {
+                ResetTimer();
+                return true;
+            }
+        }
+
+        public void ResetTimer()
+        {
+            CurrentTime = Random.Range(MinTime, MaxTime);
+        }
+    }
+
+
     private void Awake()
     {
         _combatController = GetComponent<CombatController>();
@@ -116,6 +159,44 @@ public class Player : MonoBehaviour
         inputVelocity = transform.position;
         inputVelocity.z = 0;
         transform.position = inputVelocity;
+
+
+        if (IsAI)
+        {
+            Move(MoveInput);
+
+            //if (DistanceFromPlayer() < AttackRange)
+            //{
+            //    Debug.Log("Attack");
+            //    AIAttack();
+            //}
+
+            if (CallTimer(JumpData))
+            {
+                Debug.Log("Jump");
+                Jump();
+            }
+
+            if (CallTimer(DashData))
+            {
+                Debug.Log("Dash");
+                Dash();
+            }
+
+            if (CallTimer(SkillData))
+            {
+                if (AISkillCanBeUsed())
+                {
+                    Debug.Log("Skill");
+                    Skill();
+                }
+            }
+
+            if (CallTimer(MoveData))
+            {
+                ChooseMoveDirection();
+            }
+        }
     }
 
     protected void Move(int moveInput)
@@ -188,6 +269,9 @@ public class Player : MonoBehaviour
     /// </summary>
     private void InputUpdate()
     {
+        if (IsAI)
+            return;
+
         //If the player isn't pressing either direction, make sure they aren't moving
         //Mostly a precaution
         if (Input.GetKey(Controls.Left) == false && Input.GetKey(Controls.Right) == false)
@@ -231,5 +315,100 @@ public class Player : MonoBehaviour
             _dashing = false;
             _dashInput = 0;
         }
+    }
+
+    protected virtual bool AISkillCanBeUsed()
+    {
+        //Need to override in each different character. This is the trigger for each different character to use the skill. It also has to line up with the timer hitting 0.
+        //some characters wont need triggers and therefore will just have return true
+        //example trigger
+
+        int trigger = Random.Range(0, 2);
+
+        if (trigger == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected void AIAttack()
+    {
+        FacePlayer();
+        Attack();
+    }
+
+    protected void ChooseMoveDirection()
+    {
+        MoveInput = Random.Range(-1, 2);
+
+        switch (MoveInput)
+        {
+            case -1:
+                FaceLeft();
+                break;
+
+            case 0:
+                FacePlayer();
+                break;
+
+            case 1:
+                FaceRight();
+                break;
+        }
+    }
+
+    public bool CallTimer(TimerData Timer)
+    {
+        if (!Timer.CallTimer())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    protected float DistanceFromPlayer()
+    {
+        return Vector3.Distance(transform.position, GameManager.s_p1Char.transform.position);
+    }
+
+    protected bool PlayerIsOnLeft()
+    {
+        if (GameManager.s_p1Char.transform.position.x > transform.position.x)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    protected void FacePlayer()
+    {
+        if (PlayerIsOnLeft())
+        {
+            FaceLeft();
+        }
+        else
+        {
+            FaceRight();
+        }
+    }
+
+    protected void FaceRight()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+    }
+
+    protected void FaceLeft()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
     }
 }
