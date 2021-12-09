2c28b251bb24380a4d2ada7ca0807b2ab75c6919
Luke Jones <nnblc565@gmail.com>
2021-12-09 12:20:31 +1030

    Commenting

--- Assets/Scripts/MenuStuff/SpawnPoint.cs
+++ Assets/Scripts/MenuStuff/SpawnPoint.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Empty class that exists for GetComponentInChildren since its recursive and makes it easy to find
+/// </summary>
 public class SpawnPoint : MonoBehaviour
 {
 }
