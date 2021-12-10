59cc48fb4e6a300c88b1217064ba913bb525f8b7
Luke Jones <nnblc565@gmail.com>
2021-12-10 11:21:22 +1030

    Created an entire system for projectiles

---
+++ Assets/Scripts/Combat/RangedAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : MonoBehaviour
+{
+    public void Fire(Object projectile)
+    {
+        GameObject proj = projectile as GameObject;
+
+        if (!proj)
+        {
+            Debug.LogError("Event gave invalid parameter");
+            return;
+        }
+
+        proj = Instantiate(proj, transform.position, Quaternion.LookRotation(transform.forward, Vector3.up));
+        proj.GetComponent<Bullet>().SetAttacker(transform);
+    }
+}
