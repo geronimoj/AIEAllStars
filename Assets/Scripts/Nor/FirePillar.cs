3e2e0d24a4954e48f8fd4e208ebb391a38c25d68
zachary newman <s181720@students.aie.edu.au>
2021-12-10 20:39:04 +1030

    Fire Pillar WIP

---
+++ Assets/Scripts/Nor/FirePillar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HitCollider))]
+public class FirePillar : MonoBehaviour
+{
+    HitCollider col;
+    Collider fireCollider;
+
+    public Player caster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<HitCollider>();
+
+        fireCollider = GetComponent<Collider>();
+
+        col.SetAttacker(caster.transform);
+
+        StartCoroutine(WaitBeforeExplode());
+    }
+
+    IEnumerator WaitBeforeExplode()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        Explode();
+    }
+
+    void Explode()
+    {
+        fireCollider.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Nor/InvisibleProj.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleProj : MonoBehaviour
+{
+    public float lifeTime;
+    float elapsed = 0;
+    public float speed;
+
+    public FirePillar firePillar;
+
+    [HideInInspector]
+    public Player caster;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+
+        if (elapsed > lifeTime)
+            SummonPillar();
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+            return;
+
+        //If we hit an enemy or wall, summon a fire pillar here
+
+        Player p = other.GetComponent<Player>();
+        if (p)
+        {
+            if (p == caster)
+                return;
+
+            SummonPillar();
+        }
+        else
+        {
+            if (other.CompareTag("Wall"))
+                SummonPillar();
+        }
+    }
+
+    void SummonPillar()
+    {
+        FirePillar instance = Instantiate(firePillar, transform.position, firePillar.transform.rotation);
+
+
+
+        Destroy(gameObject);
+    }
+}
