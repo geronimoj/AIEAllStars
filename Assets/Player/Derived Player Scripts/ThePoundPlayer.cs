4222bc27f320e1889f57b512910bd3aa4f7118c6
Luke Jones <nnblc565@gmail.com>
2021-12-10 10:41:41 +1030

    Updated the pound fighter to become more zombie the more damage he takes.

--- Assets/Player/Derived Player Scripts/ThePoundPlayer.cs
+++ Assets/Player/Derived Player Scripts/ThePoundPlayer.cs
@@ -6,17 +6,25 @@ public class ThePoundPlayer : Player
 {
     private float _minSpeed = 0;
 
+    private Renderer r = null;
+
     public float _maxSpeedAt0HP = 15;
 
     protected override void Start()
     {
         base.Start();
         _minSpeed = MoveSpeed;
+
+        r = GetComponentInChildren<Renderer>();
+        Material mat = Instantiate(r.material);
+        r.material = mat;
     }
 
     protected override void Update()
     {   //Scale move speed with health
         MoveSpeed = Mathf.Lerp(_maxSpeedAt0HP, _minSpeed, CurrentHealth / MaxHealth);
+        r.material.SetFloat("_Amount", CurrentHealth / MaxHealth);
+
         //Base update
         base.Update();
     }
