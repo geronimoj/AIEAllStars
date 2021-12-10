69629eb27370259ad04f7950e9ed37a9d2e06d6e
Luke Jones <nnblc565@gmail.com>
2021-12-10 19:24:58 +1030

    Updated bullet code to have explosiveness

--- Assets/Scripts/Combat/Bullet.cs
+++ Assets/Scripts/Combat/Bullet.cs
@@ -8,15 +8,24 @@ public class Bullet : HitCollider
 
     public float lifeTime = 5;
 
+    public Vector3 direction = Vector3.forward;
+
+    public bool isExplosive = false;
+
+    public HitCollider explosiveCollider = null;
+
     protected virtual void Start()
     {
         transform.SetParent(null, true);
         StartCoroutine(Kill());
+        direction.Normalize();
     }
 
     protected virtual void Update()
-    {   //Move the collider
-        transform.position -= transform.forward * (moveSpeed * Time.deltaTime);
+    {
+        Vector3 relativeVel = transform.right * direction.x + transform.up * direction.y + transform.forward * direction.z;
+        //Move the collider
+        transform.position -= relativeVel * (moveSpeed * Time.deltaTime);
     }
 
     private IEnumerator Kill()
@@ -24,5 +33,16 @@ public class Bullet : HitCollider
         yield return new WaitForSeconds(lifeTime);
 
         Destroy(gameObject);
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+
+        if (isExplosive && explosiveCollider)
+        {
+            Instantiate(explosiveCollider.gameObject, transform.position, explosiveCollider.transform.rotation);
+            isExplosive = false;
+        }
     }
 }
