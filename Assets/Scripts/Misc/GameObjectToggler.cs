9f1eac82043ff9a673167c46767122570551000c
Luke Jones <nnblc565@gmail.com>
2021-12-10 11:34:54 +1030

    Finished the Pound Fighter

---
+++ Assets/Scripts/Misc/GameObjectToggler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectToggler : MonoBehaviour
+{
+    public GameObject target;
+
+    public void ToggleOn()
+    {
+        target.SetActive(true);
+    }
+
+    public void ToggleOff()
+    {
+        target.SetActive(false);
+    }
+}
