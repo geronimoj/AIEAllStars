4ee7f0145ae1a70e0f3e846155c6324c8b106be2
Luke Jones <nnblc565@gmail.com>
2021-12-10 11:25:51 +1030

    Added spawn offset for projectiles

--- Assets/Scripts/Combat/RangedAttack.cs
+++ Assets/Scripts/Combat/RangedAttack.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RangedAttack : MonoBehaviour
 {
+    public Vector3 spawnOffset;
+
     public void Fire(Object projectile)
     {
         GameObject proj = projectile as GameObject;
@@ -14,7 +16,9 @@ public class RangedAttack : MonoBehaviour
             return;
         }
 
-        proj = Instantiate(proj, transform.position, Quaternion.LookRotation(transform.forward, Vector3.up));
+        Vector3 offset = spawnOffset.x * transform.right + spawnOffset.y * transform.up + spawnOffset.z * transform.forward;
+
+        proj = Instantiate(proj, transform.position + offset, Quaternion.LookRotation(transform.forward, Vector3.up));
         proj.GetComponent<Bullet>().SetAttacker(transform);
     }
 }
