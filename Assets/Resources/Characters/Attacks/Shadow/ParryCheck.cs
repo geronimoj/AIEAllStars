2d1bc87200af838e4a0971d80be2bca3cdc61895
Luke Jones <nnblc565@gmail.com>
2022-02-07 20:05:28 +1030

    Fix for next button appearing for clients.

--- Assets/Resources/Characters/Attacks/Shadow/ParryCheck.cs
+++ Assets/Resources/Characters/Attacks/Shadow/ParryCheck.cs
@@ -11,15 +11,26 @@ public class ParryCheck : MonoBehaviour
         attacker = me;
     }
 
+    private void Update()
+    {   //Check if the game is networked
+        if (NetworkManager.InRoom && !attacker)
+        {   //If this was spawned on an opponent, assign the attacker
+            int i = NetworkManager.AmHost ? 1 : 0;
+            //Null catch just in case
+            if (GameManager.s_instance._players[i])
+                attacker = GameManager.s_instance._players[i].transform;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         HitCollider _hit = other.GetComponent<HitCollider>();
-    
+
         if (_hit)
         {
             if (_hit.Attacker == attacker)
                 return;
-    
+
             attacker.GetComponent<ShadowstepPlayer>().CounterAttack();
         }
     }
---
+++ Assets/Scripts/Networking/AmHost.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmHost : MonoBehaviour
+{
+    public GameObject target = null;
+
+    public bool enable = false;
+
+    void Start()
+    {
+        if (NetworkManager.AmHost)
+            target.SetActive(enable);
+        else
+            target.SetActive(!enable);
+    }
+}
