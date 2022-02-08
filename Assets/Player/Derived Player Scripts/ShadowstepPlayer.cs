8ee1efdbd060761826aefb0223fb40904fd388de
Luke Jones <nnblc565@gmail.com>
2022-02-08 13:28:24 +1030

    ShadowStep is now the first character with attack priority. I'm sure nothing bad will come from this.

--- Assets/Player/Derived Player Scripts/ShadowstepPlayer.cs
+++ Assets/Player/Derived Player Scripts/ShadowstepPlayer.cs
@@ -9,6 +9,10 @@ public class ShadowstepPlayer : Player
     public Collider counterCollider;
 
     Collider _colInstance;
+    /// <summary>
+    /// Used for tracking the shadowstep is in a counter attack
+    /// </summary>
+    private bool _inCounterAttack = false;
 
     public void StartParry()
     {
@@ -31,11 +35,23 @@ public class ShadowstepPlayer : Player
 
     public void CounterAttack()
     {
+        _inCounterAttack = true;
         if (_colInstance)
             Destroy(_colInstance.gameObject);
         InvincibilityTime = 1;
+        //If we are in a networked games, force the counter animation for them.
+        if (NetworkManager.InRoom)
+            photonView.RPC("CounterOccuredOnOtherClient", Photon.Pun.RpcTarget.Others);
 
         animator.SetTrigger("Counter");
+    }
+
+    [Photon.Pun.PunRPC]
+    protected void CounterOccuredOnOtherClient()
+    {   //If the counter occured on either client, tell the other to display it/have it occur
+        //Make sure we don't re-enter the counter if we are already in/display it
+        if (!_inCounterAttack)
+            animator.SetTrigger("CounterNetwork");
     }
 
     public void CounterAttackSpawn()
@@ -53,5 +69,6 @@ public class ShadowstepPlayer : Player
         canMoveInt = 0;
         if (_colInstance)
             Destroy(_colInstance.gameObject);
+        _inCounterAttack = false;
     }
 }
